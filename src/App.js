a1e96184241c90c75e906843dbc3419615cd6c2a
Doğan Aker <dogan234@gmail.com>
2023-02-11 16:59:05 +0300

    component setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Box, Stack } from '@mui/material'
+import Navbar from "./components/navbar/Navbar";
+import Posts from './components/posts/Posts';
+import Sidebar from "./components/sidebar/Sidebar";
 
 
 const App = () => {
   return (
-    <div>
-      Hello
-    </div>
+    <Box>
+      <Navbar />
+      <Stack>
+        <Sidebar />
+        <Posts />
+      </Stack>
+    </Box>
+
   );
 }
 
